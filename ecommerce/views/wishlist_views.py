e890ea280983c2fd65369f31c6ef4dae7bb43561
oeilsimple <Oeilsimple@gmail.com>
2024-12-04 18:25:21 +0300

    optimized code

--- ecommerce/views/wishlist_views.py
+++ ecommerce/views/wishlist_views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.contrib import messages
 from ecommerce.models import Wishlist, WishlistItem, Product
 from django.contrib.auth.decorators import login_required
-from django.db import transaction
+from django.db import transaction, models
 from ecommerce.views.product_views import common_data
 from ecommerce.views.cart_views import CartService
 
@@ -34,29 +35,47 @@ def add_to_wishlist(request, product_id):
     
 @login_required(login_url='account')
 def wishlist_detail(request):
+    """
+    Display the user's wishlist.
+    """
     try:
-        wishlist = Wishlist.objects.get(user=request.user)
-        wishlist_items = wishlist.wishlist_items.select_related('product').all()
+        wishlist = Wishlist.objects.prefetch_related(
+            models.Prefetch('wishlist_items', 
+                     queryset=WishlistItem.objects.select_related('product'))
+        ).get(user=request.user)
+        
+        wishlist_items = wishlist.wishlist_items.all()
+        
         context = {
-            'w_items' : wishlist_items,
+            'w_items': wishlist_items,
             **common_data(request),
         }
+        
         template = 'wishlist.html' if wishlist_items.exists() else 'wishlist-empty.html'
         return render(request, template, context)
-    except Wishlist.DoesNotExist:
-        return render(request, 'wishlist-empty.html')
     
+    except Wishlist.DoesNotExist:
+        return render(request, 'wishlist-empty.html', {
+            **common_data(request)
+        })
+
 @login_required(login_url='account')
 def delete_wishlist_item(request, pk):
+    """
+    Remove an item from the wishlist or add to cart.
+    """
     try:
-        w_item = get_object_or_404(WishlistItem, pk=pk)
+        w_item = get_object_or_404(WishlistItem, pk=pk, wishlist__user=request.user)
+        
         if request.method == "POST":
-            if "delete" in request.POST:
-                w_item.delete()
             if "add-to-cart" in request.POST:
                 CartService.add_to_cart(request.user, w_item.product)
-                return redirect('cart_detail')
-    except:
-        pass
+                messages.success(request, "Product added to cart")
+        
+        w_item.delete()
+        messages.success(request, "Item removed from wishlist")
+    
+    except Exception as e:
+        messages.error(request, "Failed to process wishlist item")
     
     return redirect('wishlist')
