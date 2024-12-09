5e8a9947b075a7eb0cbbd6111f7a064e49072790
oeilsimple <Oeilsimple@gmail.com>
2024-12-09 19:15:48 +0300

    retrieved all the blogs and categories from db and displaying them to a template

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from blogs.models import Blog, Comment, BlogCategory
+
+def blogs(request):
+    blogs = Blog.objects.all()
+    categories = BlogCategory.objects.all()
+    context = {
+        'blogs' : blogs,
+        'b_categories' : categories
+    }
+    return render(request, 'blog.html', context)
 
 # Create your views here.
