ae90e42440ef16718891c07a2646e63a283ae16c
oeilsimple <Oeilsimple@gmail.com>
2024-12-09 19:28:36 +0300

    retrieved blogs by category

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blogs, name='blogs'),
+    path('by_cat/<cat>/', views.blogs_by_category, name='blogs_by_category'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blogs.models import Blog, Comment, BlogCategory
 
 def blogs(request):
     blogs = Blog.objects.all()
     categories = BlogCategory.objects.all()
+    context = {
+        'blogs' : blogs,
+        'b_categories' : categories
+    }
+    return render(request, 'blog.html', context)
+
+def blogs_by_category(request, cat):
+    category = get_object_or_404(BlogCategory, category=cat)
+    blogs = Blog.objects.filter(category=category)
+    categories = BlogCategory.objects.all()
+    
     context = {
         'blogs' : blogs,
         'b_categories' : categories
