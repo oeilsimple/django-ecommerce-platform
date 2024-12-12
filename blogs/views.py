f0d44ae394676a6530f61d7ef3b3d629db7811b2
oeilsimple <Oeilsimple@gmail.com>
2024-12-13 01:12:24 +0300

    created navigation for next and previous blog

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from blogs.models import Blog, Comment, BlogCategory
 from ecommerce.views.services import CommonService
+from blogs.services import BlogService
 from blogs.forms import CommentForm
 
 def blogs(request):
@@ -14,32 +15,47 @@ def blogs(request):
     return render(request, 'blog.html', context)
 
 def blogs_by_category(request, cat):
+    """
+    View to display blogs filtered by category
+    """
     category = get_object_or_404(BlogCategory, category=cat)
-    blogs = Blog.objects.filter(category=category)
-    categories = BlogCategory.objects.all()
+    blogs = BlogService.get_blogs_with_optimized_query(category)
+    categories = BlogService.get_blog_categories()
+    print(categories)
     
     context = {
-        'blogs' : blogs,
-        'b_categories' : categories,
+        'blogs': blogs,
+        'b_categories': categories,
+        'selected_category': category,
         **CommonService.get_common_context(request)
     }
     return render(request, 'blog.html', context)
 
+
 def blog(request, slug):
-    blog = get_object_or_404(Blog, slug=slug)
+    """
+    View to display blog details with navigation
+    """
+    # Get current, previous, and next blogs
+    current_blog, previous_blog, next_blog = BlogService.get_blog_with_navigation(slug)
     
+    # Handle comment submission
     if request.method == 'POST':
-        print(request.POST)
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.blog = blog
-            comment.save() 
-    comments = Comment.objects.filter(blog=blog)
-
+            comment.blog = current_blog
+            comment.save()
+            return redirect('blog_detail', slug=slug)
+    
+    comments = Comment.objects.filter(blog=current_blog).order_by('-added_at')
+    
+    # Prepare context
     context = {
-        'blog' : blog,
-        'comments' : comments,
+        'blog': current_blog,
+        'previous_blog': previous_blog,
+        'next_blog': next_blog,
+        'comments': comments,
         **CommonService.get_common_context(request)
     }
     
