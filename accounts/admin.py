5c0b60d49a74569163c33d52598189c8f248b01f
oeilsimple <Oeilsimple@gmail.com>
2024-12-23 12:42:41 +0300

    added Address to django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from accounts.models import CustomUser, ContactUs
+from accounts.models import CustomUser, ContactUs, Address
 
 admin.site.register(CustomUser)
 admin.site.register(ContactUs)
+admin.site.register(Address)
 
 # Register your models here.
