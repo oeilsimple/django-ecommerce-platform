35f67cf7d2a312a0548fd793b8d83b0a13bd6bbf
oeilsimple <Oeilsimple@gmail.com>
2025-08-24 01:17:23 +0300

    fixes03

--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input --settings=ecommerce_proj.settings.prod
-python manage.py makemigrations --settings=ecommerce_proj.settings.prod
 python manage.py migrate --settings=ecommerce_proj.settings.prod
