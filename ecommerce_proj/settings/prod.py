970aaa40a6a8119f69fb8be96627ca4048523625
oeilsimple <Oeilsimple@gmail.com>
2026-01-19 19:05:17 +0300

    fixes

--- ecommerce_proj/settings/prod.py
+++ ecommerce_proj/settings/prod.py
@@ -8,6 +8,26 @@ DEBUG = False
 ALLOWED_HOSTS += ['*']
 WSGI_APPLICATION = 'ecommerce_proj.wsgi.prod.application'
 
-DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    # Parse the database URL
+    db_config = dj_database_url.parse(DATABASE_URL)
+    
+    # Add SSL configuration for Render PostgreSQL
+    db_config['OPTIONS'] = {
+        'sslmode': 'require',
     }
+    
+    DATABASES = {
+        'default': db_config
+    }
+
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000 
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
